37e1ad7fa2f9f3e0ef979c743a69ab701fa12eea
poojitha-svg <poojitha@acentle.com>
2026-03-06 21:03:38 +0530

    Remove

--- uip-main/uip-main/services/routing-engine/dist/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.execute = execute;
-async function execute(req) {
-    return {
-        decision: "ALLOW",
-        reason: "Stub engine response"
-    };
-}
